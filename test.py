7c3296b81e130c8add4bd5827ea095644f6aefcc
jinga50334 <abcd360@gmail.com>
2015-11-17 18:00:02 +0800

    develop 2015-11-17_18:00:02

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 a=int(raw_input('Please input sth:'))
-while a >10:
-    print('int > 10')
-else:
-    print('int<10')
+l=list()
+for i in range(1,a+1):
+        l.append(i)
+        print(l)
