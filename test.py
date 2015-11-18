fb79468031411c13cab1329b311ecaa947cd7216
jinga50334 <abcd360@gmail.com>
2015-11-18 23:42:21 +0800

    develop 2015-11-18_23:42:21

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-a=int(raw_input('Please input sth:'))
-l=list()
-for i in range(1,a+1):
-        l.append(i)
-print(l)
+#a=int(raw_input('Please input sth:'))
+#l=list()
+#for i in range(1,a+1):
+#        l.append(i)
+#print(l)
+d={'a0':1,'a1':2,'a3':3,'a4':4}
+for x,y in d.items():
+    print(y)
+    print(x)
