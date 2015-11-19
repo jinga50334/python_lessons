4fb8116e7e532b80db3fa9601658726784e21c77
jinga50334 <abcd360@gmail.com>
2015-11-19 17:18:16 +0800

    develop `D`

--- test.py
+++ test.py
@@ -5,7 +5,12 @@
 #for i in range(1,a+1):
 #        l.append(i)
 #print(l)
-d={'a0':1,'a1':2,'a3':3,'a4':4}
-for x,y in d.items():
-    print(y)
-    print(x)
+#d={'a0':1,'a1':2,'a3':3,'a4':4}
+#for x,y in d.items():
+#    print(y)
+#    print(x)
+###########################
+d=dict()
+for i in range(1,4):
+    d['#',i]=i+1,'A'
+print(d)
