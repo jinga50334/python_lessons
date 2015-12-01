1f0c334fbc6e9991420de079e61d555f70b7213f
jinga50334 <abcd360@gmail.com>
2015-12-01 15:20:34 +0800

    mater 2015-12-01_15:20:34

---
+++ lessons/day4/recursion_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+x=int(raw_input('Plesea input a number:'))
+def f(x):
+    s=0
+    for i in range(1,x+1):
+        l=len(str(i))
+        s=10**l*s+i
+    return s
+print(f(x))
