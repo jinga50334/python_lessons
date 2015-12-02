72fed624cffb1900905022f3a28c7041c4bfe00b
jinga50334 <abcd360@gmail.com>
2015-12-02 11:43:51 +0800

    master 2015-12-02_11:43:51

--- lessons/day4/recursion_1.py
+++ lessons/day4/recursion_1.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 #unlimite
-x=int(raw_input('Plesea input a number:'))
+s0=int(raw_input('Plesea input a number:'))
 def f(x):
     s=0
     for i in range(1,x+1):
         l=len(str(i))
         s=10**l*s+i
-    return s
-print(f(x))
+    print(s)
+f(s0)
