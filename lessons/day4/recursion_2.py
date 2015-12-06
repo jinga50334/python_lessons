b91d1e85269e9ddefd4deff2ceeebb8e8bc99048
jinga50334 <abcd360@gmail.com>
2015-12-06 22:35:08 +0800

    master 2015-12-06_22:35:08

--- lessons/day4/recursion_2.py
+++ lessons/day4/recursion_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 n=int(raw_input('Plesea input a number:'))
-l=list(range(1,n+1))
 def f(x,y):
     return str(x)+str(y)
-print(reduce(f,l))
+print(reduce(f,range(1,n+1)))
