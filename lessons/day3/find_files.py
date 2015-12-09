813269472a2620b5edfedd60fbdd5523fce8b1be
jinga50334 <abcd360@gmail.com>
2015-12-09 17:33:33 +0800

    master

--- lessons/day3/find_files.py
+++ lessons/day3/find_files.py
@@ -6,9 +6,9 @@ Path=str(raw_input('Please input the dir_path:'))
 
 def search(Path):
     for p in os.listdir(Path):
-        if os.path.isfile(os.path.join(Path,p)):
             print(os.path.join(Path,p))
-        else:
-            search(p)
+            if os.path.isdir(os.path.join(Path,p)):
+                search(os.path.join(Path,p))
+
 
 search(Path)
