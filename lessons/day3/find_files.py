f3b860ab8827355163e8c38c913b81155b471c49
jinga50334 <abcd360@gmail.com>
2015-12-10 10:37:48 +0800

    master  2015-12-10_10:37:36

--- lessons/day3/find_files.py
+++ lessons/day3/find_files.py
@@ -2,13 +2,15 @@
 # -*- coding: UTF-8 -*-
 import os
 
-Path=str(raw_input('Please input the dir_path:'))
+#Path=str(raw_input('Please input the dir_path:'))
+Path='/home/hufu/python/python_scripts'
 
 def search(Path):
     for p in os.listdir(Path):
+        if os.path.isfile(os.path.join(Path,p)):
             print(os.path.join(Path,p))
-            if os.path.isdir(os.path.join(Path,p)):
-                search(os.path.join(Path,p))
+        if os.path.isdir(os.path.join(Path,p)):
+            search(os.path.join(Path,p))
 
 
 search(Path)
