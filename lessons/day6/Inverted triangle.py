a0c95e6f24f2e9029d43ba87bc177b96951b4c18
jinga50334 <abcd360@gmail.com>
2015-12-27 20:43:38 +0800

    Merge branch 'master' of https://github.com/jinga50334/python_lessons
    Conflicts:
    	.idea/workspace.xml

--- lessons/day6/Inverted triangle.py
+++ lessons/day6/Inverted triangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 n=int(input('Please input the num:'))
-for i in range(1,n+1):
-    print(' '*(n-i)),(' *')*i
+for i in range(-n,0):
+    print(' '*(n-abs(i))),(' *')*abs(i)
--- lessons/day6/rhombus.py
+++ lessons/day6/rhombus.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 n=int(input('Please input the num:'))
+for i in range(1,n):
+    print(' '*(n-i)),(' *')*i
 for i in range(-n,0):
     print(' '*(n-abs(i))),(' *')*abs(i)
